77d461f8c7d1bbe506b4e6437adee677527762b1
luftways <aaron@niechzial.de>
2021-07-26 17:39:50 +0200

    cleaned up client.py

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
-from datetime import datetime
+import json
 import os
+import random
+from datetime import datetime
+
 import discord
 from discord.ext import commands
 from discord.ext.commands.core import has_role
-import json
-import random
+
 import util
 
 intents = discord.Intents.default()
@@ -25,19 +27,24 @@ roles = ["villager", "werewolf", "witch", "hunter",
          "amor", "seer", "bodyguard", "wild_kid"]
 special_roles = ["witch", "hunter", "amor", "seer", "bodyguard", "wild_kid"]
 
+
 def get_time():
     return f"[{datetime.now().strftime('%H:%M:%S')}] "
 
+
 def log(level: int, message: str):
     prefix = {0: f'{util.Colours.DEBUG}[DEBUG] ',
-                1: f'{util.Colours.INFO}[INFO] ',
-                2: f'{util.Colours.WARNING}[WARNING] ',
-                3: f'{util.Colours.ERROR}[ERROR] '
-                }
+              1: f'{util.Colours.INFO}[INFO] ',
+              2: f'{util.Colours.WARNING}[WARNING] ',
+              3: f'{util.Colours.ERROR}[ERROR] '
+              }
     suffix = f'{util.Colours.END}'
     print(f'{prefix[level]}{get_time()}{message}{suffix}')
 
+
 LANGUAGE = "de-DE"
+
+
 async def load_language():
     global translations
     try:
@@ -48,13 +55,18 @@ async def load_language():
         log(3, "Language file not found. Terminating.")
         exit()
 
+
 PLAYER_FILE = "data/players.json"
+
+
 async def update_player_file():
-    with open(PLAYER_FILE,"w") as f:
-        json.dump(player_dict,f,indent=4)
+    with open(PLAYER_FILE, "w") as f:
+        json.dump(player_dict, f, indent=4)
     log(0, "Player file updated.")
 
+
 client = commands.Bot(command_prefix="-", intents=intents)
+
 
 async def game_board():
     global sent_message
@@ -67,15 +79,19 @@ async def game_board():
         sent_message = await client.get_channel(LOBBY_CHANNEL).send(message)
         game_status = None
 
+
 async def get_game_board():
     return f"**WERWOLF**\nSpieler: {len(player_list)}"
+
 
 async def get_token():
     with open("token.key") as f:
         return f.read()
 
-async def get_msg(id):    
-    return translations[id][random.choice()]
+
+async def get_msg(id):
+    return random.choice(translations[id])
+
 
 async def dm(user, msg):
     try:
@@ -83,9 +99,11 @@ async def dm(user, msg):
     except discord.Forbidden:
         log(2, f"Couldn't DM {user.name}. Error Code: Onion")
 
+
 async def dm_all(msg):
     for player in player_list:
-        dm(player, msg)
+        await dm(player, msg)
+
 
 async def assign(player, role):
     log(0, f"Assigning {role} to {player}")
@@ -101,6 +119,7 @@ async def assign(player, role):
         wild_kid = user
     await dm(user, f"You have been assigned the role **{role}**.")
 
+
 @client.event
 async def on_ready():
     log(1, "Logged in as:")
@@ -109,9 +128,11 @@ async def on_ready():
     log(1, f"--------------------")
     await client.change_presence(status=discord.Status.online)
 
+
 @client.command()
 async def version(ctx):
     await ctx.send(f"Current version: {VERSION}")
+
 
 @client.command()
 @has_role("Captain")
@@ -121,12 +142,14 @@ async def reload(ctx):
     await load_language()
     log(1, "Reload complete!")
 
+
 @client.command()
 @has_role("Captain")
 async def closeclient(ctx):
-    await client.change_presence(status=discord.Status.offline,activity=None)
+    await client.change_presence(status=discord.Status.offline, activity=None)
     os.remove(PLAYER_FILE)
     await client.close()
+
 
 @client.command()
 @has_role(GAME_MASTER)
@@ -134,6 +157,7 @@ async def opengame(ctx):
     global game_status
     game_status = "waiting"
     await game_board()
+
 
 @client.command()
 @has_role(GAME_MASTER)
@@ -143,42 +167,45 @@ async def closegame(ctx):
     game_status = "closed"
     await game_board()
     log(0, "Closing session...")
-    await client.change_presence(status=discord.Status.online,activity=None)
+    await client.change_presence(status=discord.Status.online, activity=None)
     for player in player_list:
         user = client.get_user(player)
-        await user.send(f"The game you were in was closed by the session host. Thanks for playing!\n\n_powered by Luftways_")
+        await user.send(
+            f"The game you were in was closed by the session host. Thanks for playing!\n\n_powered by Luftways_")
     log(1, "Session closed.")
+
 
 @client.command()
 async def join(ctx):
     global player_list
     global player_dict
     player = ctx.message.author
-    
+
     if game_status == "waiting":
         if ctx.message.author.id in player_list:
             await ctx.send(f"{player.mention}, you are already on the list.")
             return
         player_list.append(player.id)
         log(0, f"Updated player list: {player_list}")
-        
+
         player_dict[player.id] = {"name": f"{player.name}",
                                   "is_alive": True,
                                   "role": None,
                                   "primary": None,
                                   "secondary": None
                                   }
-        
+
         log(0, f"Updated player dictionary: {player_dict}")
         await game_board()
-        
+
         try:
             await player.send("You're in the queue for Werwolf. GLHF!")
         except discord.Forbidden:
             log(2, f"Couldn't DM {player}. Error code: EICHHÖRNCHEN")
-            
+
     elif game_status == "running":
         await ctx.send(f"{player.mention}, a game is already in progress.")
+
 
 @client.command()
 async def leave(ctx):
@@ -190,11 +217,12 @@ async def leave(ctx):
             return
         player_list.remove(ctx.message.author.id)
         log(0, f"Updated player list: {player_list}")
-        
-        player_dict.popitem(ctx.message.author.id)
+
+        player_dict.pop(ctx.message.author.id)
         log(0, f"Updated player dictionary: {player_dict}")
-        
+
         await game_board()
+
 
 @client.command()
 @has_role(GAME_MASTER)
@@ -206,46 +234,47 @@ async def start(ctx):
     global hunter
     global idol
     global wild_kid
-    
+
     game_phase = str
     wolves_list = list
     witch = str
     hunter = str
     idol = str
     wild_kid = str
-    
+
     if len(player_list) <= 0:
         await ctx.send("Not enough players in queue")
         return
     log(1, "Assigning roles.")
-    
-    await client.change_presence(status=discord.Status.dnd,activity=discord.Game(name="Werwolf"))
-    
-    max_werewolves = round(len(player_list) // 3,9)
+
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name="Werwolf"))
+
+    max_werewolves = round(len(player_list) // 3, 9)
     assigned_specials = []
-    
+
     for player in player_list:
         if random.random() < 25:
             assigned_role = "werewolf"
-            max_werewolves -1
-            await assign(player,assigned_role)
+            max_werewolves - 1
+            await assign(player, assigned_role)
             continue
         elif random.random() < 60:
             while True:
                 assigned_role = random.choice(special_roles)
                 if assigned_role not in assigned_specials:
-                    await assign(player,assigned_role)
+                    await assign(player, assigned_role)
                     assigned_specials.append(assigned_role)
                     break
                 elif assigned_role in assigned_specials and len(assigned_specials) << len(special_roles):
                     continue
                 else:
-                    await assign(player,"villager")
+                    await assign(player, "villager")
                     break
         else:
-            await assign(player,"villager")
+            await assign(player, "villager")
     log(1, f"{len(player_list)} roles assigned. Special roles: {assigned_specials}")
     await update_player_file()
+
 
 @client.command()
 @has_role(GAME_MASTER)
@@ -257,6 +286,7 @@ async def night(ctx):
     await dm_all(get_msg("sunset"))
     game_phase = "night"
 
+
 @client.command()
 @has_role(GAME_MASTER)
 async def day(ctx):
@@ -267,14 +297,17 @@ async def day(ctx):
     await dm_all(get_msg("sunrise"))
     game_phase = "day"
 
+
 @client.command()
 @has_role(GAME_MASTER)
 async def eliminate(ctx, player):
     pass
 
+
 @client.command()
 @has_role(GAME_MASTER)
 async def wolves(ctx):
     pass
+
 
 client.run(util.get_config('token'))
