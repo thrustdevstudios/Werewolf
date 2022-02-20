c394bc691f0182e199c22728d1c9f3ecdc0e064a
Aaron Niechzial <aaron@niechzial.de>
2022-02-20 20:17:50 +0100

    renamed command folder for clarity

--- client.py
+++ client.py
@@ -11,9 +11,9 @@ intents.members = True
 client = commands.Bot(command_prefix="-", intents=intents)
 
 
-for folder in os.listdir('cmd'):
-    if os.path.exists(os.path.join('cmd', folder, 'cog.py')):
-        client.load_extension(f'cmd.{folder}.cog')
+for folder in os.listdir('commands'):
+    if os.path.exists(os.path.join('commands', folder, 'cog.py')):
+        client.load_extension(f'commands.{folder}.cog')
 
 
 TOKEN = os.getenv('CLIENT_TOKEN')
