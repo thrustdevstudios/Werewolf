8e96b0deb46d69d7a951738780d79af0943018fa
aniechzial <aaron@niechzial.de>
2022-06-29 09:57:21 +0200

    added opengame command

---
+++ commands/cmd_opengame/cog.py
@@ -0,0 +1,24 @@
+from cgitb import handler
+import discord
+from discord.ext import commands
+from logic.gamehandler import GameHandler
+
+
+class OpenGameCommand(commands.Cog, name='OpenGameCommand'):
+    def __init__(self, client: commands.Bot):
+        self.client = client
+    
+    @commands.command(name='opengame')
+    @commands.guild_only
+    async def opengame(self, ctx: commands.Context):
+        handler = GameHandler()
+        handler.open()
+        await ctx.send(f'{ctx.message.author.mention} opened a game')
+    
+    @opengame.error
+    async def opengame_error(self, ctx: commands.Context, error):
+        if isinstance(error, commands.GuildNotFound):
+            await ctx.send(f'{ctx.message.author.mention} you have to be in a server to run this command')
+        else:
+            await ctx.send(f'{ctx.message.author.mention} something went wrong')
+
