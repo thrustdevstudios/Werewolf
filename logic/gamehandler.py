80fdbdd41ec6c6782ad01ea820490e8afde45dc7
aniechzial <aaron@niechzial.de>
2022-06-29 16:29:32 +0200

    changed player dict functionality

--- logic/gamehandler.py
+++ logic/gamehandler.py
@@ -9,37 +9,37 @@ data = {
 
 async def opengame(ctx: commands.Context):
     global data
-    if data.get('is_open'):
-        await ctx.send(f'{ctx.author.mention} game already open')
+    if data['is_open']:
+        await ctx.reply('game already open')
         return
     data['is_open'] = True
     await ctx.send(f'{ctx.author.mention} opened a game')
 
 async def closegame(ctx: commands.Context):
     global data
-    if not data.get('is_open'):
-        await ctx.send(f'{ctx.author.mention} there is no open game')
+    if not data['is_open']:
+        await ctx.reply('there is no open game')
         return
     data['is_open'] = False
     await ctx.send(f'{ctx.author.mention} closed the game')
 
-async def addplayer(ctx: commands.Context, user: discord.User):
+async def addplayer(ctx: commands.Context):
     global data
-    if not data.get('is_open'):
-        await ctx.send(f'{ctx.author.mention} there is no open game')
+    if ctx.author.id in data['players']:
+        await ctx.reply('player already exists')
         return
-    elif not data['players'][user.id] == None:
-        await ctx.send(f'{ctx.author.mention} is already in the game')
-    data['players'][user.id] = {
-        'name': user.name,
+    
+    data['players'][ctx.author.id] = {
+        'name': ctx.author.name,
         'role': None
     }
+
     await ctx.send(f'{ctx.author.mention} joined the game')
 
-async def removeplayer(ctx: commands.Context, user: discord.User):
+async def removeplayer(ctx: commands.Context):
     global data
     try:
-        del data['players'][user.id]
+        del data['players'][ctx.author.id]
     except:
         await ctx.send(f'{ctx.author.mention} something went wrong')
 
@@ -49,4 +49,4 @@ async def startgame(ctx: commands.Context):
         await ctx.send(f'{ctx.author.mention} is already running')
         return
     data['is_running'] = True
-    await ctx.send(f'{ctx.author.mention} started the game')
+    await ctx.reply('started the game')
