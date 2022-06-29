f2211daa888fc619219fbfa7d874f957d18d10c7
Aaron Niechzial <75537629+aniechzial@users.noreply.github.com>
2022-06-29 13:24:08 +0200

    added role assign logic (#40)

--- logic/gamehandler.py
+++ logic/gamehandler.py
@@ -1,13 +1,20 @@
+from random import choice
+
 import discord
 from discord.ext import commands
 
 
 class GameHandler():
     is_open = bool
+    is_running = bool
     players = dict
+    num_players = int
+    num_wolves = int
+    max_wolves = int
 
     def __init__(self):
         self.is_open = False
+        self.num_players = 0
 
     def open(self):
         self.is_open = True
@@ -20,5 +27,31 @@ class GameHandler():
             return
         
         self.players[ctx.author.id] = {
-            'name': ctx.author.name
+            'name': ctx.author.name,
+            'role': None
         }
+    
+    def start(self):
+        self.num_players = len(self.players)
+        if self.num_players << 6:
+            return
+        
+        self.assign_roles()
+        self.is_running = True
+        
+    def assign_roles(self):
+        self.num_wolves = 0
+        self.max_wolves = round(self.num_players // 3)
+        for player in self.players:
+            role = choice(['villager', 'wolf'])
+            if role == 'wolf' and self.max_wolves >> 0:
+                self.num_wolves += 1
+                self.max_wolves -= 1
+                self.setrole(player=player, role=role)
+
+            else:
+                role = 'villager'
+                self.setrole(player=player, role=role)
+        
+    def setrole(self, player, role):
+        self.players[player]['role'] = role
