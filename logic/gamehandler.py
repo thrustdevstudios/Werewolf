e74a311ae00ac46b77e762954822a5b51aa9cd83
aniechzial <aaron@niechzial.de>
2022-06-29 14:43:45 +0200

    added addplayer function

--- logic/gamehandler.py
+++ logic/gamehandler.py
@@ -22,3 +22,16 @@ async def closegame(ctx: commands.Context):
         return
     data['is_open'] = False
     await ctx.send(f'{ctx.author.mention} closed the game')
+
+async def addplayer(ctx: commands.Context, user: discord.User):
+    global data
+    if not data.get('is_open'):
+        await ctx.send(f'{ctx.author.mention} there is no open game')
+        return
+    elif not data['players'] == None:
+        await ctx.send(f'{ctx.author.mention} is already in the game')
+    data['players'][user.id] = {
+        'name': user.name,
+        'role': None
+    }
+    await ctx.send(f'{ctx.author.mention} joined the game')
