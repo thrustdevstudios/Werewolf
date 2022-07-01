ff8e3cee9d65e64820f6cff78aaad545a4b5c9d4
aniechzial <aaron@niechzial.de>
2022-07-01 12:37:27 +0200

    restructured commands folder

---
+++ commands/PingCommand/cog.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+
+class PingCommand(commands.Cog, name='PingCommand'):
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.command(name='ping')
+    async def ping(self, ctx: commands.Context):
+        """A command which returns a ping response.
+        Usage:
+        ```
+        -ping
+        ```
+        """
+
+        await ctx.send('Pong!')
+
+
+def setup(client: commands.Bot):
+    client.add_cog(PingCommand(client))
