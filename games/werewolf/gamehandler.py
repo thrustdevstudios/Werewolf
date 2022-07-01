31310cb21735465d1ef1480fb1e60c4b8320cbad
aniechzial <aaron@niechzial.de>
2022-07-01 14:07:11 +0200

    moved gamehandler to game folder

---
+++ games/werewolf/gamehandler.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+
+class GameHandler():
+    data = {
+        'is_open': False,
+        'is_running': False,
+        'min_players': 6,
+        'players': {
+        }
+    }
+
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    async def is_open(self) -> bool:
+        return self.data['is_open']
+    
+    async def is_running(self) -> bool:
+        return self.data['is_running']
+
+    async def get_players(self) -> dict:
+        return self.data['players']
+
+    async def get_player(self, player: str) -> dict:
+        return self.data['players'][player]
+    
+    async def add_player(self, player: str):
+        user = self.client.fetch_user(int(player))
