ab1e61ba367de556d26b07c93029c285b6a1e5d8
aniechzial <aaron@niechzial.de>
2022-07-01 13:08:27 +0200

    added dynamic importing of command classes

--- client.py
+++ client.py
@@ -1,8 +1,10 @@
+import importlib
 import os
 
 import discord
 from discord.ext import commands
 
+from logic.gamehandler import GameHandler
 
 intents = discord.Intents.default()
 intents.members = True
@@ -13,13 +15,17 @@ else:
     prefix = '.'
 
 client = commands.Bot(command_prefix=prefix, intents=intents)
+handler = GameHandler(client=client)
 
+async def register_commands():
+    for folder in os.listdir('commands'):
+        module = importlib.import_module(f'commands.{folder}.command')
+        command_class = getattr(module, f'{folder}')
+        await module.register(client=client, handler=handler)
 
 @client.event
 async def on_ready():
-    for folder in os.listdir('commands'):
-        if os.path.exists(os.path.join('commands', folder, 'cog.py')):
-            client.load_extension(f'commands.{folder}.cog')
+    register_commands()
 
 
 TOKEN = os.getenv('CLIENT_TOKEN')
