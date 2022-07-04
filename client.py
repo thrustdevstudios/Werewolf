a40850cfe5c61325d09ccf3c22bab33da6e26be9
Aaron Niechzial <aaron@niechzial.de>
2022-07-04 13:08:38 +0200

    removed class variable declaration

--- client.py
+++ client.py
@@ -20,13 +20,11 @@ handler = GameHandler(client=client)
 async def register_commands():
     for folder in os.listdir('commands'):
         module = importlib.import_module(f'commands.{folder}.command')
-        command_class = getattr(module, f'{folder}')
         await module.register(client=client)
     
 async def register_games():
     for folder in os.listdir('games'):
         handler = importlib.import_module(f'games.{folder}.gamehandler')
-        game_class = getattr(handler, f'{folder}')
         await handler.register(client=client)
 
 @client.event
